fc8dcdeb05bd76143379958974e358418b21d623
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-05-17 14:34:02 -0300

    Move dependencies to setup.py

--- scc.sh
+++ scc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scc . --exclude-dir="build,ts-boilerplate,htmlcov,dist,js,notebooks,.git,.tox,.idea,*.egg-info,.pytest_cache"
+scc . --exclude-dir="build,dist,ts-boilerplate,htmlcov,js,notebooks,.git,.tox,.idea,*.egg-info,.pytest_cache"
--- setup.py
+++ setup.py
@@ -9,4 +9,8 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     setup_requires='setuptools >= 30.3',
+    install_requires=[
+        'sidekick>=0.5.0',
+        'markupsafe~=1.0',
+    ],
 )
